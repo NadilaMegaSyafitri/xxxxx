c093c743be472556c2460f9b2fe8499255f998b9
Nadila Mega Syafitri <32352445+NadilaMegaSyafitri@users.noreply.github.com>
2018-03-24 12:14:13 +0800

    Mengisi Nilai Atribut

--- src/praktek01/BurungAksi.java
+++ src/praktek01/BurungAksi.java
@@ -11,6 +11,27 @@ package praktek01;
  */
 public class BurungAksi {
     public static void main(String[] args) {
+      Burung Merpati =  new Burung ();
+      Burung Merak =  new Burung ();
+      Burung Cendrawasih =  new Burung ();
+                
+        Merpati.paruh="Kecil Lancip";
+        Merpati.warna_bulu="Putih";
+        Merpati.habitat_asal="eropa atau afrika";
+        Merpati.warna_mata="coklat";
+        Merpati.panjang_cakar=1;
+         
+        Merak.paruh="runcing Lancip";
+        Merak.warna_bulu="biru campur hijau";
+        Merak.habitat_asal="India";
+        Merak.warna_mata="Hitam kecoklatan";
+        Merak.panjang_cakar=2;
         
+        Cendrawasih.paruh="sedang Lancip";
+        Cendrawasih.warna_bulu="kuning campur coklat";
+        Cendrawasih.habitat_asal="Indonesia";
+        Cendrawasih.warna_mata="coklat";
+        Cendrawasih.panjang_cakar=2;
+
     }
 }
