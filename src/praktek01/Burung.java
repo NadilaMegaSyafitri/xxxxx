f13a6778d934c03f9c41110ea660c7875a684d12
Nadila Mega Syafitri <32352445+NadilaMegaSyafitri@users.noreply.github.com>
2018-03-24 12:09:58 +0800

    Membuat Atribut dan Behavior

--- src/praktek01/Burung.java
+++ src/praktek01/Burung.java
@@ -10,5 +10,17 @@ package praktek01;
  * @author mega
  */
 public class Burung {
+    String paruh;
+    String warna_bulu;
+    String habitat_asal;
+    String warna_mata;
+    double panjang_cakar;
     
+    void caramakan  () {
+        System.out.println("paruh : "+paruh);
+        System.out.println("warna_bulu : "+warna_bulu);
+        System.out.println("habitat_asal : "+habitat_asal);
+        System.out.println("warna_mata : "+warna_mata);
+        System.out.println("panjang_cakar : "+panjang_cakar);
+            }
 }
